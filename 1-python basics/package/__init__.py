6859baa696445f8813a485f58adcf1c5f1f5c17b
Abhijith M <124777284+Abhijithm02@users.noreply.github.com>
2025-02-22 20:30:06 +0530

    Add files via upload

---
+++ 1-python basics/package/__init__.py
@@ -0,0 +1,4 @@
+'''__init__.py is a speciial file used in python to define
+packages and initialize their namespaces
+'''
+from.math import addition,multiplication
---
+++ 1-python basics/test.py
@@ -0,0 +1,4 @@
+from package.math import *
+
+print(addition(2,3))
+print(multiplication(4,3))
